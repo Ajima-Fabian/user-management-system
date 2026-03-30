f33957ad0f6577d0ed2861541a189bcd9b31a7cb
Ajima-Fabian <ajimafabian18@gmail.com>
2026-03-30 16:16:47 +0100

    Update create_user.sh

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -1,4 +1,4 @@
-we#!/bin/bash
+#!/bin/bash
 # Create a single user with default password
 # Forces password change at first login
 # Checks if user already exists 
@@ -6,6 +6,8 @@ we#!/bin/bash
 
 
 # Load logger
+# This command loads the logger file so its function can be used safely without breaking 
+# source loads the script while the $0 shows how the current shell us being executed, the dirname extracts the parent directory of the path to the current shell
 source "$(dirname "$0")/../utils/logger.sh"
 
 USER="$1"  # Get username from argument
@@ -16,20 +18,23 @@ if [ -z "$USER" ]; then
     exit 1
 fi
 
-# Check if user already exists
+# Check if user already exists and discard the error message
 if id "$USER" &>/dev/null; then
     echo "User $USER already exists."
+    # Pass the meeesage as parameter to the log function
     log "Skipped creation: $USER already exists"
     exit 1
 fi
 
 # Prompt for password securely
+# Pass the -s flag as to hide the password when typing
 read -s -p "Enter password for $USER: " PASSWORD
 echo
 read -s -p "Confirm password: " CONFIRM
 echo
 
 # Ensure passwords match
+# Validate the password for security 
 if [ "$PASSWORD" != "$CONFIRM" ]; then
     echo "Passwords do not match."
     log "ERROR: Passwords did not match for $USER"
